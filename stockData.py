767f38e974ab79d58684a6019046f4c80b317d4b
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2021-09-09 11:59:36 +0530

    updated somethings

--- stockData.py
+++ stockData.py
@@ -56,17 +56,17 @@ elif(interval=='60m'):
     p = 168
     f = 'h'
 elif(interval=='1d'):
-    period = '2y'
+    period = '5y'
     date_index = 'Date'
     p = 60
     f = 'd'
 elif(interval=='1wk'):
-    period = '3y'
+    period = '5y'
     date_index = 'Date'
     p = 10
     f = 'W'
 elif(interval=='1mo'):
-    period = '5y'
+    period = '10y'
     date_index = 'Date'
     p = 2
     f = 'm'
