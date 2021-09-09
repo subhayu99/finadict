041d0b9d0ca27386e9090eaca77a87fd47843105
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2021-09-10 01:11:51 +0530

    tryingSomething

--- stockData.py
+++ stockData.py
@@ -22,7 +22,7 @@ st.title('Stock Price Prediction WebApp')
 # stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
 # selected_stock = st.selectbox('Select dataset for prediction', stocks)
 st.text_input("Type in a ticker symbol (For eg. 'AAPL' for Apple Inc.)", key="selected_stock", value='AAPL')
-st.write('*Forgotten the ticker symbol?* Find it [here](https://finance.yahoo.com/lookup)')
+st.write('*Forgotten the ticker symbol?* Find it [here]("https://finance.yahoo.com/lookup")')
 comp = yf.Ticker(st.session_state.selected_stock)
 comp_info = comp.info
 if(comp_info.get('shortName')!=None):
