7772e012e6ba8ff057abd03fd78ab6d7495e29ba
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2021-09-11 14:31:20 +0530

    removed-interval

--- stockData.py
+++ stockData.py
@@ -118,7 +118,7 @@ plot_raw_data()
 def build_model():
     # Define forecasting model.
     m = Prophet(
-            interval_width=0.95, 
+            interval_width=0.75, 
             daily_seasonality=True,
             weekly_seasonality=True, 
             changepoint_prior_scale=1 
