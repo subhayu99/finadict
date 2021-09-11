0153518e14faa13904a1db7cec1587c62691f254
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2021-09-11 14:07:54 +0530

    mcmc_samples=null

--- stockData.py
+++ stockData.py
@@ -120,8 +120,8 @@ def build_model():
     m = Prophet(
             interval_width=0.95, 
             weekly_seasonality=True, 
-            changepoint_prior_scale=2, 
-            mcmc_samples = 12
+            changepoint_prior_scale=2 
+            # mcmc_samples = 12
             )
     m.add_country_holidays(country_name=comp_country_code)
     return m
