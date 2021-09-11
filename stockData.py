bf59c5d1110e70094a40bb149355300962f13623
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2021-09-11 08:46:50 +0530

    a-few-modifications

--- stockData.py
+++ stockData.py
@@ -121,7 +121,7 @@ def build_model():
     # Define forecasting model.
     m = Prophet(interval_width=0.95, 
             weekly_seasonality=True, 
-            changepoint_prior_scale=2 
+            changepoint_prior_scale=2, 
             mcmc_samples = 500
             )
     m.add_country_holidays(country_name=comp_country_code)
