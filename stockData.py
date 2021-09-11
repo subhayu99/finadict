ddae8e4311e138c749c312569c6943aae2095aca
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2021-09-11 14:07:14 +0530

    mcmc_samples=12

--- stockData.py
+++ stockData.py
@@ -121,7 +121,7 @@ def build_model():
             interval_width=0.95, 
             weekly_seasonality=True, 
             changepoint_prior_scale=2, 
-            mcmc_samples = 35
+            mcmc_samples = 12
             )
     m.add_country_holidays(country_name=comp_country_code)
     return m
