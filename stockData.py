994acfd0812f9fef7705261b624d7f9e4e6b6a22
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2021-09-15 01:14:03 +0530

    added-cross-validation

--- stockData.py
+++ stockData.py
@@ -155,5 +155,5 @@ if not (interval in interval_choices[:3]):
     show_forecast(forecast)
     data_load_state.text('Prediction done.')
 
-    df_cv = cross_validation(m, horizon = (p+' days'))
+    df_cv = cross_validation(m, horizon = (str(p)+' days'))
     st.write(df_cv)
