2b76a94cac5233dd18514b3ba8b706adaeb3cd05
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2021-09-16 22:05:01 +0530

    minor-modifications

--- stockData.py
+++ stockData.py
@@ -101,9 +101,9 @@ def main():
         comp_info = comp.info
         comp_country_code = pycountry.countries.search_fuzzy(comp_info.get('country'))[0].alpha_2
 
-        st.write('\nShowing results for**[', comp_info.get('longName'),'](comp_info.get(\'website\'))**\n')
+        st.write('\n**[', comp_info.get('longName'),'](comp_info.get(\'website\'))**\n')
         st.image(comp_info.get('logo_url'))
-        st.write('Financial Currency :', comp_info.get('financialCurrency'),'**\n')
+        st.write('Financial Currency :**', comp_info.get('financialCurrency'),'**\n')
         st.write(comp_info)
         st.write('\nyFinance\'s Recommendation :**', comp_info.get('recommendationKey'),'**\n')
         
