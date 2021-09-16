cade7f51fef87d26869b2390d07536737b8e0581
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2021-09-16 23:52:00 +0530

    minor-modifications

--- stockData.py
+++ stockData.py
@@ -64,11 +64,14 @@ def show_forecast(m, forecast, data, p, df_train):
     only_forecast = forecast # [len(data)-1:len(forecast)]
     only_forecast['Confidence (%)'] = (prediction / original) * 100
     only_forecast['Actual Price'] = df_train['y']
-    st.write(only_forecast[["ds","Actual Price","yhat","Confidence (%)","yhat_lower","yhat_upper"]].iloc[::-1])
+
+    only_forecast.rename(columns={"ds":"Datetime","yhat":"Predicted Price","yhat_lower":"Predicted Price Lower","yhat_upper":"Predicted Price Upper"}, inplace=True) 
 
     rmpse = np.sqrt(np.nanmean(np.square(((original - prediction) / original))))*100
+
     st.write('Mean Confidence Percentage =', round(only_forecast['Confidence (%)'].mean()-rmpse, 2), '%')
     st.write('Root Mean Percentage Squared Error =', round(rmpse, 5), '%')
+    st.write(only_forecast[["ds","Actual Price","yhat","Confidence (%)","yhat_lower","yhat_upper"]].iloc[::-1])
 
     st.subheader(f'Forecast plot ')
     fig1 = plot_plotly(m, forecast)
@@ -105,8 +108,7 @@ def main():
         st.image(comp_info.get('logo_url'))
         st.write('Financial Currency :**', comp_info.get('financialCurrency'),'**\n')
         st.write('\nyFinance\'s Recommendation :**', comp_info.get('recommendationKey'),'**\n')
-        st.json(comp_info)
-        #st.button('', on_click=st.write(comp_info))
+        # st.json(comp_info)
         
     elif(choice==menu[1]):
         st.title('Forex Prediction')
