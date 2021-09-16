cc1fbef97bcef40e748627427c9a3e68a5795c60
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2021-09-16 21:16:24 +0530

    minor-modifications

--- stockData.py
+++ stockData.py
@@ -103,7 +103,7 @@ def main():
 
         st.write('\nShowing results for**', comp_info.get('longName'),'**\n')
         # st.write(comp_info)
-        st.write('\nRecommendation :**', comp_info.get('recommendationKey'),'**\n')
+        st.write('\nyFinance\'s Recommendation :**', comp_info.get('recommendationKey'),'**\n')
         
     elif(choice==menu[1]):
         st.title('Forex Prediction')
