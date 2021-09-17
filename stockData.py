b14631c71278a92c17c9e10ce8568ac0e594e0b8
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2021-09-17 11:46:37 +0530

    hope-its-complete

--- stockData.py
+++ stockData.py
@@ -89,7 +89,7 @@ def show_forecast(m, forecast, data, p, df_train, currency, c2):
     act_price = only_forecast['Actual Price'].iloc[-2]
     value=str(prd_price) + ' ' + currency
     delta = str(round(((prd_price - act_price) / act_price) * 100, 2))+'% since today'
-    with c2.spinner('Predicting price...'):
+    with st.spinner('Predicting price...'):
         c2.metric(label=label, value=value, delta=delta)
 
     st.subheader('Forecast plot')
