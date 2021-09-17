f98875e01f28fae542eaf58cb9a9ab8dafe2b539
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2021-09-17 09:58:23 +0530

    minor-modifications

--- stockData.py
+++ stockData.py
@@ -121,13 +121,13 @@ def main():
         comp_country_code = pycountry.countries.search_fuzzy(comp_info.get('country'))[0].alpha_2
         currency = comp_info.get('financialCurrency')
 
-        st.write('Corp. : **[', comp_info.get('longName'),'](',comp_info.get('website'),')**')
+        st.write('Showing results for **[', comp_info.get('longName'),'](',comp_info.get('website'),')**')
         with st.expander("Company logo", expanded=False):
             if(len(comp_info.get('logo_url'))>1):
                 st.image(comp_info.get('logo_url'))
         # st.write('Financial Currency :**', comp_info.get('financialCurrency'),'**\n')
         # st.write('\nyFinance\'s Recommendation :**', comp_info.get('recommendationKey'),'**\n')
-        with st.expander("See company info...", expanded=False):
+        with st.expander("Company info.", expanded=False):
             st.json(comp_info)
         
     elif(choice==menu[1]):
