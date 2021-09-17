ea0177f2a1b04efeb9b657558c7e1ca72a554761
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2021-09-17 12:48:54 +0530

    hope-its-complete

--- stockData.py
+++ stockData.py
@@ -133,7 +133,7 @@ def main():
                 st.image(comp_info.get('logo_url'))
         # st.write('Financial Currency :**', comp_info.get('financialCurrency'),'**\n')
         # st.write('\nyFinance\'s Recommendation :**', comp_info.get('recommendationKey'),'**\n')
-        with comp_col2.expander("Company info. in JSON format", expanded=False):
+        with comp_col2.expander("Company information in JSON format", expanded=False):
             comp_info.pop("longBusinessSummary")
             st.json(comp_info)
         
