8f9259d59d396e0a03aaadcb4d9b1bdd8ee07abf
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2021-09-17 21:18:17 +0530

    hope-its-complete

--- stockData.py
+++ stockData.py
@@ -171,6 +171,7 @@ def main():
         currency = y.group().upper()
 
         st.write('\nShowing results for**', x.group().upper(), '**to**', y.group().upper(), '** coversion rate.\n')
+        st.write(f"<p style='text-decoration:none; font-size:20px'>Showing results for <strong>{x.group().upper()}</strong> to <strong>{y.group().upper()}</strong> conversion rate.</p>", unsafe_allow_html=True)
 
     form = st.sidebar.form("take parameters")
 
