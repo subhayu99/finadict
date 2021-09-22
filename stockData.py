f3e7526d36d8c85c8260411691a9aa8aef10e471
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2021-09-22 14:23:25 +0530

    hope-its-complete

--- stockData.py
+++ stockData.py
@@ -81,6 +81,7 @@ def show_forecast(m, forecast, data, p, df_train, currency, c2, selected_stock):
 
     only_forecast = forecast # [len(data)-1:len(forecast)]
     only_forecast['Confidence (%)'] = (prediction / original) * 100
+    only_forecast['Confidence (%)'].where(only_forecast['Confidence (%)'] > 100, 200 - only_forecast['Confidence (%)'], inplace=True)
     only_forecast['Actual Price'] = df_train['y']
     only_forecast['Date'] = only_forecast['ds'].astype(str)
     only_forecast['Predicted Price'] = only_forecast['yhat']
