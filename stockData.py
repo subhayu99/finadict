95459ccec7e55be4762f38ea9dc41ed315af1b75
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2021-09-23 18:41:40 +0530

    added-hour-support

--- stockData.py
+++ stockData.py
@@ -225,7 +225,7 @@ def main():
         l = 60 
         t = 'd'
         date_index = 'Datetime'
-        p = form.slider('No. of hour\'s prediction:', 1, 12, value=2, help="Try tweaking the sliders to increase accuracy.")
+        p = form.slider('No. of hour\'s prediction:', 1, 12, value=2, help="Higher no. of hours means lesser accuracy.")
         f = 'h'
     elif(interval=='1d'):
         y = form.slider('No. of months\' data to fetch:', 2, 12, value=4, help="Default value is '4' because it gives best results in most cases.")
