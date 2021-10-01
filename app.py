f71b39bb788d1d4452061d3a256488f26afc92ec
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2021-10-01 11:25:04 +0530

    Added a logo

--- app.py
+++ app.py
@@ -195,7 +195,7 @@ def main():
         initial_sidebar_state="expanded",
     )
 
-    # st.sidebar.image('FINADICT.png', width=2)
+    st.sidebar.image('finadict.png', width=2)
 
     menu = ["Stocks", "Forex", "Crypto"]
     choice = st.sidebar.selectbox("Select your market choice", menu)
